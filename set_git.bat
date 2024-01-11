a72c51134aadc6f6b66af73cad2c76d392685cda
נעמי אונקלוס-שפיגל <naomius@braude.ac.il>
2024-01-11 15:09:13 +0200

    Thu 01-11-24

--- set_git.bat
+++ set_git.bat
@@ -8,5 +8,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/naomiunk/web.git
+git remote add origin https://github.com/naomiunk/web-1.git
 git push -u origin main
